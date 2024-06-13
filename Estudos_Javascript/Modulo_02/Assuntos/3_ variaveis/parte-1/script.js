f6190ad379b5341484a7ca4f6d888e95033f01fb
fernzuldev <fernzuldev@gmail.com>
2024-06-13 16:25:32 -0300

    oitavo commit

---
+++ Estudos_Javascript/Modulo_02/Assuntos/3_ variaveis/parte-1/script.js
@@ -0,0 +1,27 @@
+let nomehtml = document.getElementById("nome")
+nomehtml.innerHTML = "Raphael Alves Ferreira Silva"
+
+let nome = "Raphael";
+
+console.log(nome);
+console.log(`O meu nome é ${nome}`);
+
+let laranjas = 5;
+
+console.log(laranjas * laranjas);
+
+nome = "joão";
+
+console.log(nome);
+
+laranjas = 3495684230;
+
+console.log(laranjas);
+
+laranjas = "Laranjas";
+
+console.log(laranjas);
+
+let um = 1, dois = 2, tres = 3;
+
+console.log(um + dois + tres);
---
+++ Estudos_Javascript/Modulo_02/Assuntos/3_ variaveis/parte-2/script.js
@@ -0,0 +1,10 @@
+var nome = "Raphael";
+
+const ip = "127.0.0.1";
+
+console.log(nome);
+console.log(ip);
+
+ip = "123213123";
+
+console.log(ip);
