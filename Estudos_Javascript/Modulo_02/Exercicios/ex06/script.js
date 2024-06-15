9f45df845e3d5d829bb0cf548d5a6b70eb6ca090
fernzuldev <fernzuldev@gmail.com>
2024-06-15 15:40:06 -0300

    decimo terceiro commit

---
+++ Estudos_Javascript/Modulo_02/Exercicios/ex06/script.js
@@ -0,0 +1,5 @@
+let nome = "Rapheael";
+let idade = 18;
+let vivo = true;
+
+console.log(`${nome}(${typeof nome}) tem ${idade}(${typeof idade}) e ${vivo}(${typeof vivo})`);
---
+++ Estudos_Javascript/Modulo_02/Exercicios/ex07/script.js
@@ -0,0 +1,11 @@
+let idade = 18;
+
+if (idade > 0) {
+    if(idade >=18){
+        console.log("Pode entrar")
+    } else {
+        console.log("Não pode")
+    }
+} else {
+    console.log("Idade invalida!")
+}
