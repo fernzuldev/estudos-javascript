c788395f0281187bfca3a4a5c2f892e5f71d239f
fernzuldev <fernzuldev@gmail.com>
2024-06-20 14:02:34 -0300

    vigesimo commit

---
+++ Estudos_Javascript/Modulo_04/Assuntos/18_push_pop/script.js
@@ -0,0 +1,10 @@
+let nomes = ["Raphael", "Alves", "Ferreira", "Silva"];
+
+let nomeRemovido = nomes.pop();
+
+console.log(nomes);
+console.log(nomeRemovido);
+
+nomes.push("Silva");
+
+console.log(nomes);
---
+++ Estudos_Javascript/Modulo_04/Assuntos/19_shift_unshift/script.js
@@ -0,0 +1,11 @@
+let nomes = ["Raphael", "Alves", "Ferreira", "Silva"];
+
+console.log(nomes);
+
+let removerNome = nomes.shift();
+
+console.log(nomes);
+
+nomes.unshift("Raphael");
+
+console.log(nomes);
